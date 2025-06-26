3cfa5886a6a673bc0a6b0351f2fcbe711f7491d5
Soyamstha <shrestha.soyam111@gmail.com>
2025-06-26 12:08:10 +0545

    add few page

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,30 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import Calculator from './Calculator';
 
-function App() {
+function Home() {
+  const navigate = useNavigate();
+
+  const goToCalculator = () => {
+    navigate('/calculator');
+  };
+
   return (
     <div>
-      <h1>calculator</h1>
-      <Calculator />
+      <h1>My React Project</h1>
+      <button onClick={goToCalculator}>Go to Calculator</button>
     </div>
   );
 }
-function Calculator() {
-  const [input, setInput] = useState('');
-  const [result, setResult] = useState('');
 
-  const handleClick = (value) => {
-    if (value === '=') {
-      try {
-        const evalResult = eval(input);
-        setResult(evalResult);
-        setInput('');
-      } catch (error) {
-        setResult('Math Error');
-        setInput('');
-      }
-    } else if (value === 'Clear') {
-      setInput('');
-      setResult('');
-    } else {
-      setInput((prev) => prev + value);
-    }
-  };
+function App() {
   return (
-    <div>
-      <div class='screen'>
-        <label>Input:</label>
-        <input value={input}></input>
-      </div>
-      <div class='screen'>
-        <label>Result:</label>
-        <input value={result}></input>
-      </div>
-      <div>
-        <div>
-          {['7', '8', '9', '/'].map((item) => (
-            <button key={item} onClick={() => handleClick(item)}>
-              {item}
-            </button>
-          ))}
-        </div>
-        <div>
-        {['4', '5', '6', '*'].map((item) => (
-          <button key={item} onClick={() => handleClick(item)}>
-            {item}
-          </button>
-        ))}
-        </div>
-        <div>
-        {['1', '2', '3', '-'].map((item) => (
-          <button key={item} onClick={() => handleClick(item)}>
-            {item}
-          </button>
-        ))}
-        </div>
-      </div>
-      <div>
-        <button onClick={() => handleClick('0')}>0</button>
-        <button onClick={() => handleClick('+')}>+</button>
-        <button className="equals" onClick={() => handleClick('=')}>=</button>
-        <button className="clear" onClick={() => handleClick('Clear')}>Clear</button>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+      </Routes>
+    </Router>
   );
 }
 
