ab3660048effddf6972613dc6a32335f225e4c2c
Soyamstha <shrestha.soyam111@gmail.com>
2025-07-02 13:25:58 +0545

    change file name because of pascal error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Calculator from './Calculator';
-import Digital_Clock from './Digital_Clock';
+import DigitalClock from './DigitalClock';
 import Joke from './joke';
-import Tic_tac_toe from './Tic_tac_toe';
+import Tictactoe from './Tictactoe';
 // import './App.css';
 import './Home.css';
 function Home() {
@@ -13,13 +13,13 @@ function Home() {
     navigate('/calculator');
   };
   const goToDigital_Clock = () => {
-    navigate('/Digital-Clock');
+    navigate('/DigitalClock');
   };
   const goToJoke = () => {
     navigate('/Joke');
   };
   const goToTik = () => {
-    navigate('/Tic_tac_toe');
+    navigate('/Tictactoe');
   };
 
   return (
@@ -41,9 +41,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calculator" element={<Calculator />} />
-        <Route path="/Digital-Clock" element={<Digital_Clock />} />
+        <Route path="/DigitalClock" element={<DigitalClock />} />
         <Route path="/Joke" element={<Joke />} />
-        <Route path="/Tic_tac_toe" element={<Tic_tac_toe />} />
+        <Route path="/Tictactoe" element={<Tictactoe />} />
       </Routes>
     </Router>
   );
